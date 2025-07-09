7cfeb862a879dba0c689294596f786da02f65c2e
CarlAugust <carlaugust.sponberg@gmail.com>
2025-07-09 18:10:27 +0200

    Clang usage and FetchContent for libraries

--- build_clang.sh
+++ build_clang.sh
@@ -1,7 +1,8 @@
-
 rm -rf build
 mkdir build && cd build
-export CC=clang
-export CXX=clang++
-cmake ..
+
+export CC=$(which clang)
+export CXX=$(which clang++)
+
+cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXXcmake ..
 make
